266922ab8f325330f28a8d9b4dd07d4ef69fba31
kafka <>
2020-09-21 19:42:44 +0800

    update

--- HCLanguageSwitch/Demo/View/ViewController.swift
+++ HCLanguageSwitch/Demo/View/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
                                 // 切换语言，参数为bundleName
                                 HCLocalizableManager.share.updateLanguage(model.bundle)
                                 
-                                print("titleWordLabel.hc_Text->%@", self?.titleWordLabel.hc_Text)
+//                                print("titleWordLabel.hc_Text->%@", self?.titleWordLabel.hc_Text)
 //                                NSLog("titleWordLabel.hc_Text->%@", self?.titleWordLabel.hc_Text)
         })
     }
@@ -49,40 +49,43 @@ class ViewController: UIViewController {
      3.使用如下方式开始文字国际化
      */
     func textLocalizeble() {
-        // 使用方式一：对语言切换感知度低的界面，采用最近宏定义取值（如界面不是常驻界面，加载必刷新，对语言切换响应感知度极低）
-        titleWordLabel.text = DEF_LOCALIZED_STRING(key: "first_inputName")
+//        // 使用方式一：对语言切换感知度低的界面，采用最近宏定义取值（如界面不是常驻界面，加载必刷新，对语言切换响应感知度极低）
+        titleWordLabel.text = DEF_LOCALIZED_STRING(key: "KG_请输入姓名")
         changeLanuageBtn.setTitle(DEF_LOCALIZED_STRING(key: "first_changeLanguage") , for: .normal)
         contentTextField.placeholder = DEF_LOCALIZED_STRING(key: "first_name")
         // 字符串拼接
-        currentLanguageLabel.text = String.init(format: DEF_LOCALIZED_STRING(key:"first_currentLanguage%@%@"), HCLocalizableResourcesFilter.share.currentLanguageName, DEF_LOCALIZED_STRING(key: "first_inputName"))
+        currentLanguageLabel.text = String.init(format: DEF_LOCALIZED_STRING(key:"first_currentLanguage%@---%@"), HCLocalizableResourcesFilter.share.currentLanguageName, DEF_LOCALIZED_STRING(key: "KG_请输入姓名"))
         
         // 使用方法二：对语言感知度高，采用分类属性方法，此方法通过关联技术，会根据语言切换立即感知，并且切换语言（如界面是常驻界面，如UITabbarController界面，切换语言不会刷新界面，但是对语言切换感知度高，需要采用分类属性方法）
-        titleWordLabel.hc_Text = "first_inputName"
+        titleWordLabel.hc_Text = "KG_请输入姓名"
         changeLanuageBtn.hc_SetTitle("first_changeLanguage", for: .normal)
         contentTextField.hc_PlaceholderSwitch = "first_name"
         // 属性方法的字符串拼接
-        currentLanguageLabel.hc_Text = "first_currentLanguage%@%@" + "&&&" + HCLocalizableResourcesFilter.share.currentLanguageName + "&&&" + "first_inputName"
-       
-        
-        /// 使用方法三，在特殊情况需要传输字符时，需要手动监听语言更新，注意：参数使用&&&进行分割
-        currentLanguageLabel.hc_Text = "first_currentLanguage%@%@" + "&&&" + HCLocalizableResourcesFilter.share.currentLanguageName + "&&&" + "first_inputName"
-        contentTextField.localizableDidChange { [weak self] in
-            self?.currentLanguageLabel.text = String.init(format: DEF_LOCALIZED_STRING(key:"first_currentLanguage%@%@"), HCLocalizableResourcesFilter.share.currentLanguageName, DEF_LOCALIZED_STRING(key: "first_inputName"))
+        currentLanguageLabel.hc_Text = "KG_当前语言：%@---%@" + "&&&" + HCLocalizableResourcesFilter.share.currentLanguageName + "&&&" + "KG_请输入姓名"
+//
+////         "first_currentLanguage%@---%@&&&中文简体&&&请输入姓名”
+//
+//
+//        /// 使用方法三，在特殊情况需要传输字符时，需要手动监听语言更新，注意：参数使用&&&进行分割
+//        currentLanguageLabel.hc_Text = "KG_当前语言：%@---%@" + "&&&" + HCLocalizableResourcesFilter.share.currentLanguageName + "&&&" + "KG_请输入姓名"
+        currentLanguageLabel.localizableDidChange { [weak self] in
+            self?.currentLanguageLabel.text = String.init(format: DEF_LOCALIZED_STRING(key:"KG_当前语言：%@---%@"), HCLocalizableResourcesFilter.share.currentLanguageName, DEF_LOCALIZED_STRING(key: "KG_请输入姓名"))
         }
     }
     
     /**
     图片国际化流程：
     1.导入LanguageSwitch文件夹内容到项目中
-    2.导入需要国际化图片，并将国际化图片根据语言区配置到xxxx.plist中，具体参考此Demo中的LocalizableImage.plist文件，注意此处的根key一定要使用国际化文件中的bundle的简写，如英语的bundle名字是“en”，中文的bundle名字是"zh-Hans"
+    2.导入需要国际化图片，并将国际化图片根据语言区配置到xxxx_zh.json中，具体参考此Demo中的LocalizableImage_en.json文件，注意新增语言时，要在HCLocalizableLanguager.swift文件中的HCLocalizableLanguage枚举新增budle名
     3.将图片统一Key配置到HCLocalizableImage中，方便读取
     4.使用功能如下方法开始图片国际化
     */
     func textLocalizebleImage() {
         // 使用方式一：对语言切换感知度低的界面，采用最近宏定义取值（如此界面不是常驻界面，加载必刷新，对语言切换响应感知度极低）
-        currentImageView.image = DEF_LOCALIZED_IMAGE(key: "HC_App_Store")
-        
-        // 使用方法二：对语言感知度高，采用分类属性方法，此方法通过关联技术，会根据语言切换立即感知，并且切换语言（如此界面是常驻界面，如UITabbarController界面，切换语言不会刷新界面，但是对语言切换感知度高，需要采用分类属性方法）
+//        currentImageView.image = DEF_LOCALIZED_IMAGE(key: "HC_App_Store")
+//        currentImageView.image = UIImage.init(named: DEF_LOCALIZED_IMAGE_STRING(key: "HC_App_Store"))
+//
+//        // 使用方法二：对语言感知度高，采用分类属性方法，此方法通过关联技术，会根据语言切换立即感知，并且切换语言（如此界面是常驻界面，如UITabbarController界面，切换语言不会刷新界面，但是对语言切换感知度高，需要采用分类属性方法）
         currentImageView.hc_Image = "HC_App_Store"
     }
     
